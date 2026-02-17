8d32865babdccf6ab8cddb65cae7968028ea2467
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-02-17 16:08:37 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::time::Duration;
+
+use chromiumoxide::browser::BrowserConfigBuilder;
+use chromiumoxide::Browser;
+use futures::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let (mut browser, mut handler) = Browser::launch(
+        BrowserConfigBuilder::default()
+            .with_head()
+            .build()?,
+    )
+    .await?;
+
+    let handle = tokio::spawn(async move {
+        while let Some(h) = handler.next().await {
+            if h.is_err() {
+                break;
+            }
+        }
+    });
+
+    let page = browser.new_page("https://www.google.com").await?;
+
+    println!("Browser opened! Press Enter to close...");
+    std::io::stdin().read_line(&mut String::new()).ok();
+
+    browser.close().await?;
+    handle.await?;
+
+    Ok(())
+}
